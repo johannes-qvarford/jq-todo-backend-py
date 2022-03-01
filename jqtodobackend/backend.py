305f7651a16b9a451d02fcf52d26dbf2193b8118
Johannes Qvarford <johannes.qvarford@lynkco.com>
2022-03-01 09:05:23 +0100

    Refactored delete() to use repository.

--- jqtodobackend/backend.py
+++ jqtodobackend/backend.py
@@ -1,14 +1,7 @@
 import uuid
 
-from fastapi import FastAPI, HTTPException, Depends
-from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
 
-from jqtodobackend.db import (
-    find_todo,
-    patch_todo,
-    remove_todo,
-    get_db,
-)
 from jqtodobackend.models import Todo, CreatedTodo, TodoChanges
 from jqtodobackend.repository import TodoRepository
 
@@ -44,17 +37,18 @@ def get(
     return repo.find(_id).as_successful_http_response_model()
 
 
-@app.patch("/{_id}", response_model=CreatedTodo)
-def patch(_id: uuid.UUID, todo_changes: TodoChanges, db: Session = Depends(get_db)):
-    todo = find_todo(db, _id)
-    if not todo:
-        raise HTTPException(status_code=404, detail="Todo not found")
-    patch_todo(db, _id, todo_changes)
-    return todo
+@app.patch("/{_id}")
+def patch(
+    _id: uuid.UUID,
+    todo_changes: TodoChanges,
+    repo: TodoRepository = Depends(TodoRepository),
+):
+    repo.patch(_id, todo_changes)
 
 
 @app.delete("/{_id}")
-def delete(_id: uuid.UUID, db: Session = Depends(get_db)):
-    todo = find_todo(db, _id)
-    if todo:
-        remove_todo(db, _id)
+def delete(
+    _id: uuid.UUID,
+    repo: TodoRepository = Depends(TodoRepository),
+):
+    repo.delete(_id)
--- jqtodobackend/repository.py
+++ jqtodobackend/repository.py
@@ -4,16 +4,29 @@ from jqtodobackend.db import *
 
 
 class TodoQueryResult:
-    def __init__(self, todo):
+    def __init__(self, session, todo):
+        self.session = session
         self._todo = todo
 
     def as_successful_http_response_model(self):
         return self._todo
 
+    def patch(self, todo_changes):
+        patch_todo(self.session, self._todo.id, todo_changes)
+
+    def delete(self):
+        remove_todo(self.session, self._todo.id)
+
 
 class MissingTodoQueryResult:
     def as_successful_http_response_model(self):
         raise HTTPException(status_code=404, detail="Todo not found")
+
+    def patch(self, *args, **kwargs):
+        pass
+
+    def delete(self, *args, **kwargs):
+        pass
 
 
 class TodoRepository:
@@ -31,4 +44,14 @@ class TodoRepository:
 
     def find(self, _id):
         todo = find_todo(self.session, _id)
-        return TodoQueryResult(todo) if todo is not None else MissingTodoQueryResult()
+        return (
+            TodoQueryResult(self.session, todo)
+            if todo is not None
+            else MissingTodoQueryResult()
+        )
+
+    def patch(self, _id, todo_changes):
+        self.find(_id).patch(todo_changes)
+
+    def delete(self, _id):
+        self.find(_id).delete()
