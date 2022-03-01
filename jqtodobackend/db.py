f903b85c84c665cb6a9abbb57dc62da8cf5e3876
Johannes Qvarford <johannes.qvarford@lynkco.com>
2022-03-01 14:29:36 +0100

    Replaced old-style code for SqlAlchemy forward compatibility.

--- jqtodobackend/db.py
+++ jqtodobackend/db.py
@@ -12,6 +12,7 @@ engine = create_engine(
     connect_args={"check_same_thread": False},
     echo=True,
     poolclass=StaticPool,
+    future=True,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- jqtodobackend/repository.py
+++ jqtodobackend/repository.py
@@ -1,18 +1,29 @@
 from fastapi import Depends
 from fastapi.encoders import jsonable_encoder
+from sqlalchemy import select, update, delete
 from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
 
 from jqtodobackend.db import *
 
 
+class QueryResult:
+    @staticmethod
+    def of(session, obj: Todo | None):
+        return (
+            TodoQueryResult(session, obj)
+            if obj is not None
+            else MISSING_TODO_QUERY_RESULT
+        )
+
+
 class TodoQueryResult:
-    def __init__(self, session, todo):
+    def __init__(self, session, todo: Todo):
         self.session = session
         self._todo = todo
 
     def as_http_response(self):
-        return JSONResponse(jsonable_encoder(self._todo))
+        return JSONResponse(jsonable_encoder(self._todo.as_created_todo))
 
 
 class MissingTodoQueryResult:
@@ -29,7 +40,10 @@ class TodoRepository:
         self.session = session
 
     def all(self):
-        return [row.as_created_todo for row in self.session.query(Todo).all()]
+        return [
+            row.as_created_todo
+            for row in self.session.execute(select(Todo)).scalars().all()
+        ]
 
     def clear(self):
         self.session.query(Todo).delete()
@@ -40,24 +54,18 @@ class TodoRepository:
         self.session.commit()
 
     def find(self, _id):
-        found_todos = [
-            x.as_created_todo for x in self.session.query(Todo).filter_by(id=str(_id))
-        ]
-
-        return (
-            TodoQueryResult(self.session, found_todos[0])
-            if len(found_todos) > 0
-            else MISSING_TODO_QUERY_RESULT
+        return QueryResult.of(
+            session=self.session, obj=self.session.get(Todo, str(_id))
         )
 
     def patch(self, _id, todo_changes):
-        (
-            self.session.query(Todo)
-            .filter_by(id=str(_id))
-            .update({k: v for k, v in todo_changes if v is not None})
+        self.session.execute(
+            update(Todo)
+            .where(Todo.id == str(_id))
+            .values(**{k: v for k, v in todo_changes if v is not None})
         )
         self.session.commit()
 
     def delete(self, _id):
-        self.session.query(Todo).filter_by(id=str(_id)).delete()
+        self.session.execute(delete(Todo).where(Todo.id == str(_id)))
         self.session.commit()
